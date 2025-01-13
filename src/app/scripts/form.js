1639f3063e0de93301c80b414d4e08226352516f
Саша Егорова <aleksa.smile@icloud.com>
2025-01-13 14:58:09 +0300

    changed

--- src/app/scripts/form.js
+++ src/app/scripts/form.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("#form"); 
 const inputs = document.querySelectorAll("input");
-const nameInput = document.querySelector("#name");
-console.log(nameInput);
+const nameInput = document.querySelector("#name");//вытаскиваем все инпуты по тегу (селектору)
+// console.log(inputs); //получаем массив всех инпутов
 const phoneInput = document.querySelector("#phone");
 const emailInput = document.querySelector("#mail");
 
@@ -61,11 +61,15 @@ function checkNameValidity() {
     if (!isValidValue) { 
         nameInput.classList.add("input--invalid"); 
         nameInput.setCustomValidity("Имя должно содержать только буквы русского или латинского алфавита");
-        nameInput.reportValidity();
-        return; 
+        nameInput.reportValidity();//тут возня с тем что у нас по факту идет пошагово сначала нативная валидация, потом императивно руками мы еще раз проверяем
+        //они срабатывают не одновременно
+        //ты как бы засетала туда сообщение, но чтобы его показать тебе надо дернуть либо еще раз нативную проверку, либо руками вывести сообщение
+
+        return false; 
     };
 
     nameInput.classList.remove("input--invalid");
+    return true;
 };
 
 function checkPhoneValidity() {
@@ -77,10 +81,11 @@ function checkPhoneValidity() {
         phoneInput.setCustomValidity("Номер телефона должен состоять из цифр и/или символов (), +, -");
         phoneInput.reportValidity();
 
-        return;
+        return false;
     };
 
     phoneInput.classList.remove("input--invalid");
+    return true;
 };
 
 function checkEmailValidity() {
@@ -90,10 +95,11 @@ function checkEmailValidity() {
     if (!isValidValue) {
         emailInput.classList.add("input--invalid");
         
-        return;
+        return false;
     };
 
     emailInput.classList.remove("input--invalid");
+    return true;
 };
 
 
@@ -113,54 +119,55 @@ function clearInputs() {
         },
     };
 
+//вместо nameInput.value = ""; .....
+//можно вытащить все инпуты через querySelectorAll и пробежаться по ним циклом - Можно закрыть кейс с тем, что список инпутов может поменяться
+//Цикл для обнуления значений всех инпутов:
     inputs.forEach(input => {
         input.value = "";
         input.classList.remove("input--invalid");
         input.setCustomValidity("");
     });
 
-};
-
-function validateInput () {
-    checkNameValidity();
-    checkPhoneValidity();
-    checkEmailValidity();
-
-    // const isAllInputsValid = Object.values(formData).filter(
-    //     (value) => !value.isValid 
-    // ).length;
-
-};
-
-// Функция для обработки ввода в инпут
-function checkInputValidity(event) { 
-    validateInput(event.target);
-
-    // checkNameValidity(event.target);
-    // checkPhoneValidity(event.target);
-    // checkEmailValidity(event.target);
-
-    const isAllInputsValid = Object.values(formData).filter(
-                (value) => !value.isValid 
-            ).length;
+    // nameInput.value = "";
+    // phoneInput.value = "";
+    // emailInput.value = "";
 
 };
 
 
-// Добавляем обработчики на каждый инпут
-inputs.forEach((input) => {
-    input.addEventListener("input", checkInputValidity);
-});
+
+// Функция для валидации инпутов
+const validateInputs = () => {
+    const validators = {
+        name: checkNameValidity,
+        phone: checkPhoneValidity,
+        email: checkEmailValidity,
+    };
+
+    const inputsArray = Array.from(inputs);
+    inputsArray.map((input) => { //Метод map() создаёт новый массив с результатом вызова указанной функции для каждого элемента массива.
+        return validators[input.id];
+    })
+    
+    return inputsArray.every(Boolean);
+};
+
+// в validateInputs мы просто создаем массив из inputs, потому что NodeList !== Array и мы можем только через forEach там проитерироваться, что нам неудобно
+// мапимся по результирующему массиву и на каждом вызове вместо элемента просто возвращаем результат проверки по айдишнику инпута, используя словарик validators
+// Из всей функции возвращаем true если все валидны и false если хоть один невалиден
+
+
 
 
 //корректировка function checkFormValidity()  по замечаниям:
 // Функция-обработчик для отправки формы:
 function handleFormSubmit(event) {
-    event.preventDefault(); 
+    event.preventDefault(); // предотвращаем стандартное поведение (перезагрузку)отправки формы
 
-    const formNativeValidity = form.checkValidity();
-
-    const isValidForm = inputs.every(validateInput);
+    const formNativeValidity = form.checkValidity(); 
+//form.checkValidity(); - у формы есть такая встроенная функция (ее не надо нигде выше описывать - она встроенная). Она проверяет нативную валидацию (встроенную), которая у нас есть в html в инпутах (мы например задали required, minlength, maxlength) 
+    const isValidForm = validateInputs();//возвращает булин тип
+    console.log(isValidForm);
 
     if (formNativeValidity && isValidForm ) {  
         console.log({
@@ -173,14 +180,9 @@ function handleFormSubmit(event) {
     };
 };
 
-
-
 form.addEventListener("submit", handleFormSubmit);
 
 
-// form.addEventListener("submit", (event) => {
-//     event.preventDefault();
-    // checkFormValidity();
-// });
+
 
 
