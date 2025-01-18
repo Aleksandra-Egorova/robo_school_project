8d4fe982e1c834cd510712c56d4becce0086b4ae
Саша Егорова <aleksa.smile@icloud.com>
2025-01-18 16:33:17 +0300

    fixed code

--- src/app/scripts/modal.js
+++ src/app/scripts/modal.js
@@ -1,77 +1,61 @@
-
-const openModalBtns = document.querySelectorAll(".teacher-info__more");
-const closeModalBtn = document.querySelector(".close-modal-btn");
-const modal = document.querySelector(".modal");
-const tabButtons = document.querySelectorAll(".nav-tabs__btn");
-const tabsContent = document.querySelectorAll(".tabs-content__info");
-
+const openModalBtns = document.querySelectorAll('.teacher-info__more');
+const closeModalBtn = document.querySelector('.modal__close-btn');
+const modal = document.querySelector('.modal');
+const tabButtons = document.querySelectorAll('.nav-tabs__btn');
+const tabsContent = document.querySelectorAll('.tabs-content__info');
 
 const closeModal = () => {
-    modal.classList.remove('modal--open');
-    document.body.classList.remove('body--noscroll');
+  modal.classList.remove('modal--open');
+  document.body.classList.remove('body--noscroll');
 
-    tabsContent.forEach((tab) => {
-        tab.classList.remove('tabs-content__info--active'); 
-    });
+  tabsContent.forEach(tab => {
+    tab.classList.remove('tabs-content__info--active');
+  });
 
-    tabButtons.forEach((tabBtn) => {
-        tabBtn.classList.remove('nav-tabs__btn--active');
-    });
+  tabButtons.forEach(tabBtn => {
+    tabBtn.classList.remove('nav-tabs__btn--active');
+  });
 };
 
+const handleCloseModalClick = event => {
+  const isElementCloseModal = event.target === modal || event.target.closest('.modal__close-btn') || event.code === 'Escape';
 
-const handleCloseModalClick = (event) => {
-    if (
-        event.target === modal || 
-        event.target.closest('.close-modal-btn') ||
-        event.code === 'Escape'
-    ) {
-        closeModal();
+  if (!isElementCloseModal) {
+    return;
+  };
 
-        window.removeEventListener('keydown', handleCloseModalClick); 
-    };
+  closeModal();
 };
-
 
 const openModal = () => {
-    modal.classList.add('modal--open'); 
-    document.body.classList.add('body--noscroll'); 
-    window.addEventListener('keydown', handleCloseModalClick); 
-    
-    tabButtons[0].classList.add('nav-tabs__btn--active');
-    tabsContent[0].classList.add('tabs-content__info--active');
+  modal.classList.add('modal--open');
+  document.body.classList.add('body--noscroll');
+
+  tabButtons[0].classList.add('nav-tabs__btn--active');
+  tabsContent[0].classList.add('tabs-content__info--active');
 };
 
 openModalBtns.forEach(button => {
-    button.addEventListener('click', openModal);
+  button.addEventListener('click', openModal);
 });
 
+tabButtons.forEach(function (tabButton) {
+  tabButton.addEventListener('click', function () {
+    tabsContent.forEach(tab => {
+      tab.classList.remove('tabs-content__info--active');
+    });
+
+    tabButtons.forEach(tabBtn => {
+      tabBtn.classList.remove('nav-tabs__btn--active')
+    });
+
+    const activeTabBtn = tabButton;
+    activeTabBtn.classList.add('nav-tabs__btn--active');
+
+    const activeTabsContent = document.querySelector('#' + tabButton.dataset.tab);
+    activeTabsContent.classList.add('tabs-content__info--active');
+  });
+});
 
 modal.addEventListener('click', handleCloseModalClick);
-
-
-
-tabButtons.forEach(function(tabButton) {
-    tabButton.addEventListener('click', function() {
-
-        tabsContent.forEach((tab) => { 
-            tab.classList.remove('tabs-content__info--active'); 
-        });
-
-        tabButtons.forEach((tabBtn) => {
-            tabBtn.classList.remove('nav-tabs__btn--active');
-        })
-
-        const activeTabBtn = tabButton;
-        activeTabBtn.classList.add('nav-tabs__btn--active');
-
-        const activeTabsContent = document.querySelector("#" + tabButton.dataset.tab);
-        activeTabsContent.classList.add('tabs-content__info--active');
-    });
-});
-
-
-
-
-
-
+window.addEventListener('keydown', handleCloseModalClick);
