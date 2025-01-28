1b1b725f04622b4d1375adf3d6a7df3a4db9c37a
Саша Егорова <aleksa.smile@icloud.com>
2025-01-28 12:47:53 +0300

    changed functionality for burger menu, added anchor-target for nav sections

--- src/app/scripts/burger_menu.js
+++ src/app/scripts/burger_menu.js
@@ -2,16 +2,27 @@ const burgerMenu = document.querySelector('.burger-menu');
 const openBurgerMenuBtn = document.querySelector('.mobile-btns__burger');
 const closeBurgerMenuBtn = document.querySelector('.burger-menu__close');
 const burgerMenuLinks = document.querySelectorAll('.list-item__link');
+const sectionAbout = document.querySelector('.about');
+const sectionTeachers = document.querySelector('.teachers');
+const sectionPrices = document.querySelector('.prices');
 
-const openAndCloseBurgerMenu = () => {
-  burgerMenu.classList.toggle('burger-menu--open');
-  document.body.classList.toggle('body--noscroll');
+const openBurgerMenu = () => {
+  burgerMenu.classList.add('burger-menu--open');
+  document.body.classList.add('body--noscroll');
+  sectionAbout.classList.add('about__anchor-target');
+  sectionTeachers.classList.add('teachers__anchor-target');
+  sectionPrices.classList.add('prices__anchor-target');
 };
 
-openBurgerMenuBtn.addEventListener('click', openAndCloseBurgerMenu);
+const closeBurgerMenu = () => {
+  burgerMenu.classList.remove('burger-menu--open');
+  document.body.classList.remove('body--noscroll');
+};
 
-closeBurgerMenuBtn.addEventListener('click', openAndCloseBurgerMenu);
+openBurgerMenuBtn.addEventListener('click', openBurgerMenu);
+
+closeBurgerMenuBtn.addEventListener('click', closeBurgerMenu);
 
 burgerMenuLinks.forEach((link) => {
-  link.addEventListener('click', openAndCloseBurgerMenu);
+  link.addEventListener('click', closeBurgerMenu);
 });
