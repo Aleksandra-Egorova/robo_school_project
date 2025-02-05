a44be4d9f631b11b9ff08a2a8cd2353b1a8ac70c
Саша Егорова <aleksa.smile@icloud.com>
2025-02-05 11:23:24 +0300

    changed

--- src/app/scripts/modal.js
+++ src/app/scripts/modal.js
@@ -55,28 +55,9 @@ openModalBtns.forEach((button) => {
   button.addEventListener('click', openModal);
 });
 
-tabButtons.forEach((tabButton) => {
-  tabButton.addEventListener('click', () => {
-    tabsContent.forEach((tab) => {
-      tab.classList.remove('tabs-content__info--active');
-    });
-
-    tabButtons.forEach((tabBtn) => {
-      tabBtn.classList.remove('nav-tabs__btn--active');
-    });
-
-    const activeTabBtn = tabButton;
-    activeTabBtn.classList.add('nav-tabs__btn--active');
-
-    const activeTabsContent = document.querySelector(`#${tabButton.dataset.tab}`);
-    activeTabsContent.classList.add('tabs-content__info--active');
-  });
-});
-
 modal.addEventListener('click', handleCloseModalClick);
 window.addEventListener('keydown', handleCloseModalClick);
 
-//Dropdown
 const openDropdown = () => {
   dropdownArrow.classList.add('dropdown-btn__arrow--open');
   dropdownMenu.classList.add('dropdown-menu--open');
@@ -92,54 +73,74 @@ dropdownBtn.addEventListener('click', () => {
     dropdownArrow.classList.contains('dropdown-btn__arrow--open') &&
     dropdownMenu.classList.contains('dropdown-menu--open')
   ) {
+    closeDropdown();
     return;
-  };
+  }
 
   openDropdown();
 });
 
-dropdownTabButtons.forEach((dropdownTabBtn) => {
-  dropdownTabBtn.addEventListener('click', closeDropdown);
-});
-
-const rermoveActiveTabsContent = (tabsContent) => {
+const removeActiveFromTabs = () => {
   tabsContent.forEach((tab) => {
     tab.classList.remove('tabs-content__info--active');
   });
 };
 
-const removeActiveButtons = (dropdownTabButtons) => {
-  dropdownTabButtons.forEach((dropdownTabBtn) => {
-    dropdownTabBtn.classList.remove('dropdown-menu__btn--active');
+const removeActiveFromTabButtons = ({ isDropdown }) => {
+  if (isDropdown) {
+    dropdownTabButtons.forEach((dropdownTabBtn) => {
+      dropdownTabBtn.classList.remove('dropdown-menu__btn--active');
+    });
+
+    return;
+  }
+
+  tabButtons.forEach((tabBtn) => {
+    tabBtn.classList.remove('nav-tabs__btn--active');
   });
 };
 
-const addActiveButtons = (button) => {
-  button.classList.add('dropdown-menu__btn--active');
+const setActiveTabBtn = ({ tabButton, isDropdown }) => {
+  if (isDropdown) {
+    tabButton.classList.add('dropdown-menu__btn--active');
+    return;
+  }
+
+  tabButton.classList.add('nav-tabs__btn--active');
 };
 
-const addActiveTabsContent = (button) => {
-  const activeTabsContent = document.querySelector(`#${button.dataset.tab}`);
+const setActiveTabContent = (dataTab) => {
+  const activeTabsContent = document.querySelector(`#${dataTab}`);
   activeTabsContent.classList.add('tabs-content__info--active');
 };
 
+const handleSelectTab = (tabButton) => () => {
+  removeActiveFromTabs();
+  removeActiveFromTabButtons({ isDropdown: false });
+  setActiveTabBtn({ tabButton, isDropdown: false });
+  setActiveTabContent(tabButton.dataset.tab);
+};
+
+tabButtons.forEach((tabButton) => {
+  tabButton.addEventListener('click', handleSelectTab(tabButton));
+});
+
+const createSelectDropdownOptionHandler = (optionBtn) => () => {
+  closeDropdown();
+  textSpan.textContent = optionBtn.textContent;
+  removeActiveFromTabs();
+  removeActiveFromTabButtons({ isDropdown: true });
+  setActiveTabBtn({ tabButton: optionBtn, isDropdown: true });
+  setActiveTabContent(optionBtn.dataset.tab);
+};
+
 dropdownTabButtons.forEach((dropdownTabBtn) => {
-  dropdownTabBtn.addEventListener('click', () => {
-    rermoveActiveTabsContent(tabsContent);
-    removeActiveButtons(dropdownTabButtons);
-    addActiveButtons(dropdownTabBtn);
-    addActiveTabsContent(dropdownTabBtn);
-  });
+  dropdownTabBtn.addEventListener(
+    'click',
+    createSelectDropdownOptionHandler(dropdownTabBtn),
+  );
 });
 
-// Изменяем текст кнопки дропдауна на текст выбранного элемента
-dropdownTabButtons.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    textSpan.textContent = btn.textContent;
-  });
-});
-
-// При клике вне селекта, если он открыт, он закрывается
 const handleCLoseDropdownClick = (event) => {
   if (!event.target.closest('.dropdown')) {
     closeDropdown();
